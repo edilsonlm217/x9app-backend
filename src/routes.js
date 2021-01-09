6030688aeea162d0da7bb3fd3477415eb638413e
Edilson Lima <edilsonlm217@hotmail.com>
2021-01-09 13:33:57 -0400

    Implementação dos endpoints da API

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
+import ClientController from './app/controllers/ClientController';
+import SearchController from './app/controllers/SearchController';
+import InvoiceController from './app/controllers/InvoiceController';
+
+const routes = new Router();
+
+routes.post('/session', SessionController.store);
+
+routes.post('/users', UserController.store);
+
+routes.post('/clients', ClientController.store);
+routes.get('/clients', ClientController.show);
+
+routes.get('/search', SearchController.index);
+
+routes.post('/invoice', InvoiceController.store);
+routes.delete('/invoice', InvoiceController.destroy);
+routes.put('/invoice', InvoiceController.update);
+
+
+
+export default routes;
